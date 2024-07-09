c78da98787ca9774f7f6ecd65a8012b1fb5e052b
DiNg <dinhquangnguyen1997@gmail.com>
2024-07-09 11:07:18 +0700

    refactor: 💡 add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Banner from '@/components/banner'
 import Header from '@/components/Header'
 import Layout from '@/components/layout'
 
@@ -18,27 +19,7 @@ export default function Home() {
             <p>Blogs</p>
           </div>
         </section>
-        <aside>
-          <h2 className="mb-2 text-lg font-bold">Banners</h2>
-          <div className="mb-4 border p-4">
-            <p>1% Down & 2% Grant</p>
-            <a href="#" className="font-bold text-blue-500">
-              Learn More
-            </a>
-          </div>
-          <div className="mb-4 border p-4">
-            <p>$7,500 Grant</p>
-            <a href="#" className="font-bold text-blue-500">
-              Learn More
-            </a>
-          </div>
-          <div className="border p-4">
-            <p>Loan Officer Info Card</p>
-            <a href="#" className="font-bold text-blue-500">
-              Contact Now
-            </a>
-          </div>
-        </aside>
+        <Banner />
       </div>
     </Layout>
   )
---
+++ src/components/banner.tsx
@@ -0,0 +1,25 @@
+export default function Banner() {
+  return (
+    <aside>
+      <h2 className="mb-2 text-lg font-bold">Banners</h2>
+      <div className="mb-4 border p-4">
+        <p>1% Down & 2% Grant</p>
+        <a href="#" className="font-bold text-blue-500">
+          Learn More
+        </a>
+      </div>
+      <div className="mb-4 border p-4">
+        <p>$7,500 Grant</p>
+        <a href="#" className="font-bold text-blue-500">
+          Learn More
+        </a>
+      </div>
+      <div className="border p-4">
+        <p>Loan Officer Info Card</p>
+        <a href="#" className="font-bold text-blue-500">
+          Contact Now2
+        </a>
+      </div>
+    </aside>
+  )
+}
