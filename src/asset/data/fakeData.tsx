8e16855f35b245bc76e5dda9ea6f82ae7a5bd125
DiNg <dinhquangnguyen1997@gmail.com>
2024-07-16 11:32:18 +0700

    feat: 🎸 add shadcn components

--- src/asset/data/fakeData.tsx
+++ src/asset/data/fakeData.tsx
@@ -2,7 +2,7 @@ export const blogs = [
   {
     title: 'Beautiful Family House for Rent',
     author: 'John Doe',
-    date: '07 - 01 - 2024',
+    date: 'July 1st, 2024',
     category: 'Real Estate',
     language: 'English',
     imageUrl: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be',
@@ -12,7 +12,7 @@ export const blogs = [
   {
     title: 'Modern Apartment in the City Center',
     author: 'Jane Smith',
-    date: '06 - 20 - 2024',
+    date: 'June 20th, 2024',
     category: 'Real Estate',
     language: 'English',
     imageUrl: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914',
@@ -22,7 +22,7 @@ export const blogs = [
   {
     title: 'Cozy Cottage Near the Beach',
     author: 'Alice Johnson',
-    date: '06 - 15 - 2024',
+    date: 'June 15th, 2024',
     category: 'Real Estate',
     language: 'English',
     imageUrl: 'https://images.unsplash.com/photo-1497366216548-37526070297c',
