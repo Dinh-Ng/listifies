8740260b9da21931073b3b77f983cf390329727e
DiNg <dinhquangnguyen1997@gmail.com>
2024-07-19 15:41:28 +0700

    feat: 🎸 refactor code to use Shadcn component

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import houseImg from '@/asset/img/house.webp'
 import { Mail, Phone } from 'lucide-react'
 
+import { Card } from '@/components/ui/card'
 import Banner from '@/components/banner'
 import Header from '@/components/Header'
 import Layout from '@/components/layout'
@@ -10,16 +11,18 @@ import Layout from '@/components/layout'
 export default function DetailBlog() {
   return (
     <Layout>
-      <Header href={'blogs'} />
-      <div className="mx-auto">
-        {/* Main Content */}
-        <main className="relative py-2">
-          <div className="h-32 w-full bg-[#fef5da] px-2 pt-2">
+      <Header href={'/blogs'} />
+      {/*<div className="mx-auto">*/}
+      {/* Main Content */}
+      <main className="bg-muted/40 w-full py-2">
+        <div className="flex h-32 w-full flex-col items-center bg-[#fef5da] px-2 pt-2">
+          <div className="w-full px-4 md:px-6 lg:w-10/12">
             <h1 className="mb-4 text-3xl font-bold">
               Blog Detail Page - Post Title
             </h1>
-            <div className="grid grid-cols-3 gap-4">
-              <div className="col-span-2">
+            <div className="grid gap-4 md:grid-cols-[280px_1fr] lg:grid-cols-[350px_1fr]">
+              <AuthorInfo />
+              <div className="grid">
                 <section className="mb-6 rounded">
                   <div className="relative">
                     <div className="absolute left-2 top-2 flex items-center space-x-4">
@@ -52,36 +55,42 @@ export default function DetailBlog() {
                   </p>
                 </article>
               </div>
-              <aside className=" bg-white">
-                <div className="mb-4 items-center space-x-4 rounded border p-4">
-                  <div className="flex">
-                    <img
-                      src="https://placehold.co/100x100"
-                      alt="Author Image"
-                      className="size-16 rounded-full"
-                    />
-                    <div className="ml-2 text-sm">
-                      <h2 className="text-xl font-bold">Author Name</h2>
-                      <p>Job Title</p>
-                      <p>License#: 1234567</p>
-                    </div>
-                  </div>
-
-                  <div className="flex">
-                    <Mail />
-                    <p className="ml-2">author@gmail.com</p>
-                  </div>
-                  <div className="flex">
-                    <Phone />
-                    <p className="ml-2">(123) 456-7890</p>
-                  </div>
-                </div>
-                <Banner />
-              </aside>
             </div>
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
+      {/*</div>*/}
     </Layout>
+  )
+}
+
+function AuthorInfo() {
+  return (
+    <aside className=" bg-white">
+      <Card className="mb-4 items-center space-x-4 rounded border p-4">
+        <div className="flex">
+          <img
+            src="https://placehold.co/100x100"
+            alt="Author Image"
+            className="size-16 rounded-full"
+          />
+          <div className="ml-2 text-sm">
+            <h2 className="text-xl font-bold">Author Name</h2>
+            <p>Job Title</p>
+            <p>License#: 1234567</p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <Mail />
+          <p className="ml-2">author@gmail.com</p>
+        </div>
+        <div className="flex">
+          <Phone />
+          <p className="ml-2">(123) 456-7890</p>
+        </div>
+      </Card>
+      <Banner />
+    </aside>
   )
 }
