66c68d1fc86619445f47d5c4dd2344ccc60722fe
DiNg <dinhquangnguyen1997@gmail.com>
2024-08-03 22:24:58 +0700

    fix: 🐛 add more text example

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -20,7 +20,7 @@ export default function DetailBlog() {
             <div className="w-full px-2 lg:w-10/12 lg:px-4">
               <div className="grid gap-4 md:grid-cols-[1fr_280px] lg:grid-cols-[1fr_350px]">
                 <div className="">
-                  <h1 className="mb-2 text-3xl font-bold">
+                  <h1 className="mb-2 line-clamp-1 text-3xl font-bold">
                     Blog Detail Page - Post Title
                   </h1>
                   <div className="text-base font-bold text-[#505050]">
@@ -50,13 +50,43 @@ export default function DetailBlog() {
                     </div>
                   </section> */}
                   <section />
-                  <article className="prose mt-10 max-w-none">
-                    <p className="rounded bg-white p-2">
+                  <article className="prose mt-10 max-w-none rounded bg-white p-2">
+                    <p>
                       <strong>Lorem Ipsum</strong> is simply dummy text of the
                       printing and typesetting industry. Lorem Ipsum has been
                       the industry&apos;s standard dummy text ever since the
                       1500s, when an unknown printer took a galley of type and
                       scrambled it to make a type specimen book. It has
+                    </p>
+                    <p>
+                      It is a long established fact that a reader will be
+                      distracted by the readable content of a page when looking
+                      at its layout. The point of using Lorem Ipsum is that it
+                      has a more-or-less normal distribution of letters, as
+                      opposed to using Content here, content here`, making it
+                      look like readable English. Many desktop publishing
+                      packages and web page editors now use Lorem Ipsum as their
+                      default model text, and a search for lorem ipsum will
+                      uncover many web sites still in their infancy. Various
+                      versions have evolved over the years, sometimes by
+                      accident, sometimes on purpose (injected humour and the
+                      like).
+                    </p>
+                    <p>
+                      Contrary to popular belief, Lorem Ipsum is not simply
+                      random text. It has roots in a piece of classical Latin
+                      literature from 45 BC, making it over 2000 years old.
+                      Richard McClintock, a Latin professor at Hampden-Sydney
+                      College in Virginia, looked up one of the more obscure
+                      Latin words, consectetur, from a Lorem Ipsum passage, and
+                      going through the cites of the word in classical
+                      literature, discovered the undoubtable source. Lorem Ipsum
+                      comes from sections 1.10.32 and 1.10.33 of de Finibus
+                      Bonorum et Malorum (The Extremes of Good and Evil) by
+                      Cicero, written in 45 BC. This book is a treatise on the
+                      theory of ethics, very popular during the Renaissance. The
+                      first line of Lorem Ipsum, Lorem ipsum dolor sit amet..,
+                      comes from a line in section 1.10.32.
                     </p>
                   </article>
                 </div>
