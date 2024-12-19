43cb068f43500b7751f7591b51b992d831243975
DiNg <dinhquangnguyen1997@gmail.com>
2024-12-19 20:55:34 +0700

    fix: 🐛 remove BrowserSafeComponent

--- src/app/add-listing/home-for-lease/page.tsx
+++ src/app/add-listing/home-for-lease/page.tsx
@@ -1,20 +1,8 @@
-import dynamic from 'next/dynamic'
-
 import AddListing from '../add-listing'
-
-// Dynamically import the component with browser APIs
-const BrowserSafeComponent = dynamic(
-  () =>
-    import('@/components/browser-safe').then((mod) => mod.BrowserSafeComponent),
-  { ssr: false } // This ensures the component only renders on client-side
-)
 
 const AddHomeLease = () => {
   return (
-    <>
-      <AddListing embed_link="https://airtable.com/embed/appoSlkiXV4WhKEq1/pag3KadZGP2Wf17rf/form" />
-      <BrowserSafeComponent />
-    </>
+    <AddListing embed_link="https://airtable.com/embed/appoSlkiXV4WhKEq1/pag3KadZGP2Wf17rf/form" />
   )
 }
 
