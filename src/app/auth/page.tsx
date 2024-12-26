5b5cc8d272c358f6237fc076bcaf274e10c41914
DiNg <dinhquangnguyen1997@gmail.com>
2024-12-27 00:25:29 +0700

    fix: 🐛 auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,29 +2,36 @@
 
 import { useState } from 'react'
 
+import { IsClientCtxProvider, useIsClient } from '@/hooks/is-client-ctx'
 import { Button } from '@/components/ui/button'
 import Login from '@/components/auth/Login'
 import Register from '@/components/auth/Register'
 
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true)
+  const isClient = useIsClient()
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md space-y-8">
-        {isLogin ? <Login /> : <Register />}
-        <div className="text-center">
-          <Button
-            variant="link"
-            onClick={() => setIsLogin(!isLogin)}
-            className="font-medium text-indigo-600 hover:text-indigo-500"
-          >
-            {isLogin
-              ? 'Need an account? Register'
-              : 'Already have an account? Login'}
-          </Button>
+    <IsClientCtxProvider>
+      {isClient && (
+        <div className="flex min-h-screen items-center justify-center bg-gray-100">
+          <div className="w-full max-w-md space-y-8">
+            {isLogin ? <Login /> : <Register />}
+            <div className="text-center">
+              <Button
+                variant="link"
+                onClick={() => setIsLogin(!isLogin)}
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                {isLogin
+                  ? 'Need an account? Register'
+                  : 'Already have an account? Login'}
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </IsClientCtxProvider>
   )
 }
 
