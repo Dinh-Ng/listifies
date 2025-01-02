948b8562b5fa87a180c6c3fabce20d67d8ce1364
DiNg <dinhquangnguyen1997@gmail.com>
2025-01-03 01:18:19 +0700

    fix: 🐛 display toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import '@/styles/globals.css'
 
 import { AuthProvider } from '@/contexts/AuthContext'
 
+import { Toaster } from '@/components/ui/toaster'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -21,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
+        <Toaster />
       </body>
     </html>
   )
